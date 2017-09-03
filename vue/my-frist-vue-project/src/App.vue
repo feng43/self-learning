ccb6cdae01e90a19618c2f9c50b966f8148d69f8
sunshifeng <sunshifeng0@164.com>
2017-09-03 19:57:39 +0800

    add vue demo

---
+++ vue/my-frist-vue-project/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div id="app">
+    <!-- img src="./assets/logo.png">
+    <Hello></Hello> -->
+    <h1 v-text="title"></h1>
+    <h1 v-html="title2"></h1>
+    <input v-model="newItem" type="text" @keyup.enter="addNew">   <!-- v-model 只限制使用在input textarea select中 -->
+    <ul>
+      <li v-for="item in items" v-text="item.label" :class="{finished:item.isFinished}" @click="toggleFinish(item)"></li> <!-- v-bind:class与:class相同   @click与v-on:click相同-->
+    </ul>
+    <!-- <components-a :msgfromfather='ssf' @child-tell-me-something="listenToMyBoy"></components-a> -->
+    <components-a msgfromfather='ssf' @child-tell-me-something="listenToMyBoy"></components-a>
+    <!-- 驼峰转成小写中线格式,定义  v-on:child-tell-me-something -->
+
+
+    <!-- dispatch event -->
+    <!-- <components-a msgfromfather="you die"></components-a> -->
+    <p>{{ childWords }}</p>
+    <!-- <Hello></Hello> -->
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+import componentsA from './components/componentsA';
+import Store from "./store";
+console.log(Store);
+export default {
+  // name: 'app',
+  components: {
+    Hello,
+    componentsA
+  },
+  data () {  // 等价于 data: function (){}
+    return {
+      title : "this is a tudo list",
+      title2 : "<span>???</span>this is a tudo list",
+      items : //[
+        // {
+        //   label: "coding",
+        //   isFinished: false
+        // },
+        // {
+        //   label: "walking",
+        //   isFinished: true
+        // }
+        Store.fetch(),
+      //],
+      newItem: "",
+      childWords: "",
+      ssf:""
+    }
+  },
+  watch: {
+    items: {
+      handler (val, oldVal) {
+        Store.save(val);
+      },
+      deep: true //为false items数组只有一个key更改的话取不到
+    }
+  },
+  // events: {
+  //   listenToMyBoy (msg) {
+  //     this.childWords = msg;
+  //   }
+  // },
+  methods: {//methods 对象
+    toggleFinish (item) {
+      console.log(item);
+      item.isFinished = !item.isFinished;
+    },
+    addNew () {
+      console.log(this.newItem);
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      });
+      this.newItem = "";
+      this.ssf = "sunshifeng";
+      //this.$broadcast('onAddnew', this.items);
+    },
+    listenToMyBoy (msg) {
+      this.childWords = msg;
+    }
+  }
+}
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue!'
+//   }
+// })
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.finished {
+  text-decoration: underline;
+}
+</style>
---
+++ vue/my-frist-vue-project/src/components/componentsA.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
+    <h1>{{ msgfromfather }}</h1>
+    <button @click="onClickMe">click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to componentA',
+      msg2: this.msgfromfather
+    }
+  },
+  // events: {
+  //   onAddnew (items) {
+  //     console.log(items);
+  //   }
+  // },
+  props: [ "msgfromfather" ],//数组
+  methods: {
+    onClickMe () {
+      console.log(this.msgfromfather);
+      this.$emit("child-tell-me-something",this.msg);
+      //this.$dispatch("child-tell-me-something",this.msg);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
