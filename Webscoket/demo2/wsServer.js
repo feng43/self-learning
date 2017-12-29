d8afce96d1be21243599f811c9a6436befb3e8c6
sunshifeng <sunshifeng@huigo8.com>
2017-12-29 16:15:07 +0800

    add websocket

---
+++ Webscoket/demo2/wsServer.js
@@ -0,0 +1,20 @@
+var ws = require("nodejs-websocket")
+
+var PORT = 8001
+
+var server = ws.createServer(function(conn){
+	console.log('New connection')
+	conn.on("text", function(str){
+		console.log("Received " + str)
+		conn.sendText(str)
+	})
+	conn.on("close", function(code, reason){
+		console.log("Connection closed")
+	})
+	conn.on("error", function(err) {
+		console.log("header err")
+		console.log(err)
+	})
+}).listen(PORT)
+
+console.log("websocket server listening on port " + PORT)
---
+++ Webscoket/demo3/wsServer.js
@@ -0,0 +1,32 @@
+var ws = require("nodejs-websocket")
+
+var PORT = 8001
+
+var clientCount = 0
+
+var server = ws.createServer(function(conn){
+	console.log('New connection')
+	clientCount++
+	conn.nickname = 'user' + clientCount
+	broadcast(conn.nickname + ' comes in')
+	conn.on("text", function(str){
+		console.log("Received " + str)
+		broadcast(str)
+	})
+	conn.on("close", function(code, reason){
+		console.log("Connection closed")
+		broadcast(conn.nickname + ' left')
+	})
+	conn.on("error", function(err) {
+		console.log("header err")
+		console.log(err)
+	})
+}).listen(PORT)
+
+console.log("websocket server listening on port " + PORT)
+
+function broadcast(str){
+	server.connections.forEach(function(connection) {
+		connection.sendText(str);
+	})
+}
